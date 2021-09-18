383556e548bcab13872d22e26d3c74d8cc20f8d0
thanakritcankha <68786794+thanakritcankha@users.noreply.github.com>
2021-09-18 23:18:39 +0700

    Create index.js

---
+++ INT201--63130500045-MyWorks/Practices/index.js
@@ -0,0 +1,11 @@
+let hundred = 100;
+let fola = 2.5;
+let alpha = 'A';
+let beta = 'B';
+let ceta = 'C5';
+
+console.log(hundred);
+console.log(fola);
+console.log(alpha);
+console.log(beta);
+console.log(ceta);
